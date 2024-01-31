5bc07198a5b135e65e85dec6ee8d650ff6e2483d
Dmitry Krasnov <atlanticl@yandex.ru>
2024-01-31 20:39:24 +0300

    refactoring brain-gcd, brain-prime

--- brain_games/scripts/games/brain_gcd.py
+++ brain_games/scripts/games/brain_gcd.py
@@ -13,15 +13,12 @@ def main():
         number_1 = random.randint(1, 100)
         number_2 = random.randint(1, 100)
         expression = f'{number_1} {number_2}'
-        gd_1 = []
-        for number1 in range(1, number_1 + 1):
-            if number_1 % number1 == 0:
-                gd_1.append(number1)
         correct_answer = ''
-        for n in reversed(gd_1):
-            if number_2 % n == 0:
-                correct_answer = str(n)
-                break
+        for n in range(number_1, 0, -1):
+            if number_1 % n == 0:
+                if number_2 % n == 0:
+                    correct_answer = str(n)
+                    break
         count += testing.test(expression, correct_answer)
     print(f'Congratulations, {name}!') if count == 3 else print(f'Let\'s try again, {name}')
 
