6574d8010f832bbdcdf1ba4f3cfc6846a70e23f8
Dmitry Krasnov <atlanticl@yandex.ru>
2024-01-31 20:39:24 +0300

    refactoring brain-gcd, brain-prime

--- brain_games/scripts/games/brain_prime.py
+++ brain_games/scripts/games/brain_prime.py
@@ -7,15 +7,18 @@ def main():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    print('Find the greatest common divisor of given numbers.')
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
     count = 0
     while count < 3:
         number = random.randint(1, 100)
-        i = 0
-        for n in range(1, number + 1):
-            if number % n == 0:
-                i += 1
-        correct_answer = 'no' if i > 2 or number == 1 else 'yes'
+        if number == 1:
+            correct_answer = 'no'
+        else:
+            i = 0
+            for n in range(1, number + 1):
+                if number % n == 0:
+                    i += 1
+            correct_answer = 'no' if i > 2 else 'yes'
         count += testing.test(number, correct_answer)
     print(f'Congratulations, {name}!') if count == 3 else print(f'Let\'s try again, {name}')
 
