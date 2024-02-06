8e482fe1a5330da336854c00397cc4e3117f151f
Dmitry Krasnov <atlanticl@yandex.ru>
2024-02-06 07:33:23 +0300

    corrections after review

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -8,7 +8,7 @@ from brain_games.games import greetings
 def find_num_in_progression(count):
     start = random.randint(1, 100)
     step = random.randint(1, 10)
-    num_of_elements = random.randint(3, 10)
+    num_of_elements = random.randint(5, 10)
     progression = list(range(start, start + step * num_of_elements, step))
     index = random.randint(0, len(progression) - 1)
     correct_answer = str(progression[index])
